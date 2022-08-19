bb1d581568601b20e68b2afb7d5285e0c733873c
kumaranand <kumaranand@vmware.com>
2022-08-19 15:58:30 +0530

    Fix integration test
    - Create secret

--- test/integration/crd_rw_test.go
+++ test/integration/crd_rw_test.go
@@ -24,6 +24,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -44,6 +45,7 @@ var _ = Describe(fmt.Sprintf("%s,%s: Basic CRD Read-Write", focusAws, focusAzure
 
 	var (
 		namespace     v1.Namespace
+		secret        *corev1.Secret
 		account       *v1alpha1.CloudProviderAccount
 		cloudProvider string
 
@@ -116,6 +118,14 @@ var _ = Describe(fmt.Sprintf("%s,%s: Basic CRD Read-Write", focusAws, focusAzure
 				},
 			}
 		}
+		// SDK call will automatically base64 encode data.
+		secret = &corev1.Secret{
+			ObjectMeta: v12.ObjectMeta{
+				Name:      accountParameters.SecretRef.Name,
+				Namespace: accountParameters.SecretRef.Namespace,
+			},
+			Data: map[string][]byte{accountParameters.SecretRef.Key: []byte(accountParameters.SecretRef.Credential)},
+		}
 	}
 
 	createNS := func() {
@@ -131,8 +141,11 @@ var _ = Describe(fmt.Sprintf("%s,%s: Basic CRD Read-Write", focusAws, focusAzure
 	}
 	createAccount := func() {
 		setCloudAccount()
+		logf.Log.Info("Create", "secret", secret.Name)
+		err := k8sClient.Create(context.TODO(), secret)
+		Expect(err).ToNot(HaveOccurred())
 		logf.Log.Info("Create", "account", account.Name)
-		err := k8sClient.Create(context.TODO(), account)
+		err = k8sClient.Create(context.TODO(), account)
 		Expect(err).ToNot(HaveOccurred())
 		time.Sleep(5 * time.Second)
 	}
